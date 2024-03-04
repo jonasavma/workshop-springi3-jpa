37312a51a82556fc376b5203499bfc3909e82e14
Jonas Avelino <jonasavma@gmail.com>
2024-03-04 20:13:41 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/br/com/educandoweb/entities/PedidoItem.java
@@ -0,0 +1,84 @@
+package br.com.educandoweb.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import br.com.educandoweb.entities.pk.PedidoItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "pedido_item")
+public class PedidoItem implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private PedidoItemPK id;
+
+	private Integer quantidade;
+	private Double preco;
+
+	public PedidoItem() {
+	}
+
+	public PedidoItem(Pedido pedido, Produto produto, Integer quantidade, Double preco) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PedidoItem other = (PedidoItem) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
