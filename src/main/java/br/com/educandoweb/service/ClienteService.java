320076941855faf6cb69bd8bb13fe01e090d072d
Jonas Avelino <jonasavma@gmail.com>
2024-03-09 00:03:51 -0300

    User update

--- src/main/java/br/com/educandoweb/service/ClienteService.java
+++ src/main/java/br/com/educandoweb/service/ClienteService.java
@@ -23,16 +23,26 @@ public class ClienteService {
 		Optional<Cliente> obj = repository.findById(id);
 		return obj.get();
 	}
-	
-	
+
 	public Cliente insert(Cliente obj) {
 		return repository.save(obj);
-		
-		
+
 	}
 
-	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	public Cliente update(Long id, Cliente obj) {
+		Cliente entity = repository.getReferenceById(id);
+		updateDate(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateDate(Cliente entity, Cliente obj) {
+
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
